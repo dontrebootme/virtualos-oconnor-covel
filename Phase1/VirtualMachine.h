0ba86678bcd99f23852412a9b1bd3604db7af4d6
covel.tim <covel.tim@390a1010-54d3-94d7-ee63-aec3611083fc>
2010-04-17 17:10:23 +0000

    starting VM header

--- Phase1/VirtualMachine.h
+++ Phase1/VirtualMachine.h
@@ -7,7 +7,36 @@
 
 #ifndef _VIRTUALMACHINE_H
 #define	_VIRTUALMACHINE_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector> 
+#include <map>
 
+using namespace std;
+
+class format1 {
+	public:
+		unsigned UNUSED:6;//UNUSED[5:0]
+		unsigned RS:2;		//RS[7:6]
+		unsigned I:1;		//I[8]
+		unsigned RD:2;		//RD[10:9]
+		unsigned OP:5;		//OP[15:11]
+	};
+
+class format2 {
+	public:
+		unsigned ADDR:8;	//ADDR[7:0]
+		unsigned I:1;		//I[8]
+		unsigned RD:2;		//RD[10:9]
+		unsigned OP:5;		//OP[15:11]
+	};
+	
+union instruction {
+	int i;
+	format1 f1;
+	format2 f2;
+	};
 
 
 #endif	/* _VIRTUALMACHINE_H */
