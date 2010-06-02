d463f0dcea904340ee72bfc8a30a6c8ac42a64da
patrick@theoconnor.com <patrick@theoconnor.com@390a1010-54d3-94d7-ee63-aec3611083fc>
2010-06-02 21:29:07 +0000

    OS.h Edits

--- trunk/Phase3/os.h
+++ trunk/Phase3/os.h
@@ -20,6 +20,19 @@ private:
         list<PCB *> pcb, terminateJob;
         queue<PCB *> readyQ, waitQ, runQ;
         PCB * running;
+
+	struct InvertedPage
+	{
+		string frameOwner;
+		int page;
+	};
+	vector<InvertedPage *> invertPageTable; 
+	queue<int> FIFORef;
+	
+	void printInfo(), checkWaiting(), loadPage(PCB * p);
+	void checkKillFrames(stack<int>), closeStreams(), idle(), nextJob();
+	bool checkPage(int, PCB *), FIFO, LRU;
+	int contextSwitch(), getEmptryFrame(), idleTime, idleCounter;
 };
 
 #endif
